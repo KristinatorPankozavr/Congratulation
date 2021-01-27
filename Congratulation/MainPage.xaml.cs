918a511223cd86856d12f57736747c7fe1de78f9
Kristina Pankova <Kristina Pankova@WIN-CUR9MCP5QTE>
2021-01-27 06:17:25 +0200

    Добавьте файлы проекта.

---
+++ Congratulation/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Congratulation
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rnd;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+         private void ButtonSend_Clicked_1(object sender, EventArgs e)
+        {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                emailMessenger.SendEmail($"{picker.SelectedItem}", $"{EntrySubject}", $"{EntryRecipients}");
+            }
+            ButtonSend.Text = "SENT!";
+        }
+
+        private void randombutton_Clicked_1(object sender, EventArgs e)
+        {
+            picker.Title = "You choosed: " + picker.Items[picker.SelectedIndex];
+            if (picker.SelectedIndex == 0)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 1)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 2)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 3)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+            if (picker.SelectedIndex == 4)
+            {
+                EntrySubject.Text = $"{picker}";
+            }
+        }
+    }
+}
